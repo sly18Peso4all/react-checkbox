8285da5bb6d2c198e334466b6ae5bd448e25a675
sly18Peso4all <jomasco18@gmail>
2021-04-23 23:01:37 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [dropdown, setDropdown] = useState("apple");
+  const [isChecked, setIsChecked] = useState(false);
+  const [radio, setRadio] = useState("apple");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <h2>Drop Down Value is : {dropdown}</h2>
+      <h2>Checkbox is : {isChecked ? "True" : "False"}</h2>
+      <h2>Radio button is : {radio}</h2>
+
+      <select
+        value={dropdown}
+        onChange={(e) => {
+          setDropdown(e.target.value);
+        }}
+      >
+        <option value="apple">apple</option>
+        <option value="orange">Orange</option>
+        <option value="pineapple">Pineapple</option>
+        <option value="mangoes">Mangoes</option>
+      </select>
+
+      <label>CheckBox : </label>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={(e) => {
+          setIsChecked(e.target.checked);
+        }}
+      />
+      <label>Apple : </label>
+      <input
+        type="radio"
+        value="apple"
+        checked={radio === "apple"}
+        onChange={(e) => {
+          setRadio(e.target.value);
+        }}
+      />
+      <label>Orange : </label>
+      <input
+        type="radio"
+        value="orange"
+        checked={radio === "orange"}
+        onChange={(e) => {
+          setRadio(e.target.value);
+        }}
+      />
+      <label>Birne: </label>
+      <input
+        type="radio"
+        value="birne"
+        checked={radio === "birne"}
+        onChange={(e) => {
+          setRadio(e.target.value);
+        }}
+      />
+    </form>
   );
 }
 
